3941e40c85e267f68429ee650b062c04b285ac28
awesome-button <oleshko.lena@gmail.com>
2020-10-01 18:28:44 +0200

    First implementation of ProjectCollection class

--- src/jetbrains/Main.java
+++ src/jetbrains/Main.java
@@ -3,14 +3,21 @@ package jetbrains;
 public class Main {
 
     public static void main(String[] args) {
+
+        ProjectCollection collection = new ProjectCollection();
+        Project project1 = new Project("Travel to Asia");
+        Project project2 = new Project("Learn programming");
+
 	    Task task1 = new Task("buy a map", "30102020", "Travel to Asia");
         Task task2 = new Task("book hotels", "30102020", "Travel to Asia");
 
-        Project project = new Project("Travel to Asia");
-        project.addTask(task1);
-        project.addTask(task2);
-        System.out.println(project.listTasks());
-        project.removeTask(1);
-        System.out.println(project.listTasks());
+        project1.addTask(task1);
+        project2.addTask(task2);
+
+        collection.addProject(project1);
+        collection.addProject(project2);
+
+        System.out.println(collection.listProjects());
+
     }
 }
--- src/jetbrains/TaskManager.java
+++ src/jetbrains/TaskManager.java
@@ -1,4 +1,31 @@
 package jetbrains;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TaskManager {
+
+    ProjectCollection collection = new ProjectCollection();
+    Scanner scanner;
+
+    public TaskManager() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+
+        System.out.println("Enter command:");
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command == "list projects") {
+
+            }
+        }
+
+
+
+    }
+
+
 }
