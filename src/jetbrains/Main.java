433c679b98edbeb62d7f088e74296b01079c39d8
awesome-button <oleshko.lena@gmail.com>
2020-10-01 17:39:15 +0200

    First implementation of class Task added

--- src/jetbrains/Main.java
+++ src/jetbrains/Main.java
@@ -3,6 +3,10 @@ package jetbrains;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Task task = new Task("buy a map", "30102020", "Travel to Asia");
+        System.out.println(task);
+        System.out.println(task.getProject());
+        System.out.println(task.getDueDate());
+        System.out.println(task.getTitle());
     }
 }
--- src/jetbrains/Task.java
+++ src/jetbrains/Task.java
@@ -1,4 +1,41 @@
 package jetbrains;
+import java.util.HashMap;
 
 public class Task {
+    HashMap<String, String> taskDetails;
+    boolean isDone;
+
+    public Task(String title, String dueDate, String project) {
+        this.taskDetails = new HashMap<>();
+        this.taskDetails.put("title", title);
+        this.taskDetails.put("dueDate", dueDate);
+        this.taskDetails.put("project", project);
+        this.isDone = false;
+    }
+
+    public String getTitle() {
+        return this.taskDetails.get("title");
+    }
+
+    public String getDueDate() {
+        return this.taskDetails.get("dueDate");
+    }
+
+    public String getProject() {
+        return this.taskDetails.get("project");
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void formatDate() {
+
+    }
+
+    public String toString() {
+        return "Task: " + this.taskDetails.get("title") + "\nDue date: " + this.taskDetails.get("dueDate")
+                + "\nProject: " + this.taskDetails.get("project");
+    }
+
 }
